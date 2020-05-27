051f728df8c9d95dd48da1995fb0a6b8fafb72db
RainbowMango <renhongcai@huawei.com>
2020-05-27 10:40:49 +0800

    Add automatically http request

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"net/http"
 	"os"
 	"sort"
 	"strings"
@@ -15,6 +16,7 @@ type metrics struct {
 	endpoint string
 	file     string
 	report   string
+	url      string
 }
 
 func RecordReport(report string, problems []promlint.Problem) {
@@ -48,42 +50,49 @@ func main() {
 		{
 			name:     "kube-apiserver",
 			endpoint: "/metrics", // curl localhost:8080/metrics
+			url:      "http://localhost:8080/metrics",
 			file:     "data/apimetrics",
 			report:   "report/kube-apiserver.log",
 		},
 		{
 			name:     "kube-scheduler",
 			endpoint: "/metrics", // curl localhost:10251/metrics
+			url:      "http://localhost:10251/metrics",
 			file:     "data/kubescheduler",
 			report:   "report/kube-scheduler.log",
 		},
 		{
 			name:     "kube-proxy",
 			endpoint: "/metrics", // curl localhost:10249/metrics
+			url:      "http://localhost:10249/metrics",
 			file:     "data/kubeproxy",
 			report:   "report/kube-proxy.log",
 		},
 		{
 			name:     "kubelet-resource-v1alpha1",
 			endpoint: "/metrics/resource/v1alpha1", // curl localhost:10255/metrics/resource/v1alpha1
+			url:      "http://localhost:10255/metrics/resource/v1alpha1",
 			file:     "data/kubeletresourcev1alpha1",
 			report:   "report/kubelet-resource-v1alpha1.log",
 		},
 		{
 			name:     "kubelet-resource",
 			endpoint: "/metrics/resource", // curl localhost:10255/metrics/resource
+			url:      "http://localhost:10255/metrics/resource",
 			file:     "data/kubeletresource",
 			report:   "report/kubelet-resource.log",
 		},
 		{
 			name:     "kubelet-probes",
 			endpoint: "/metrics/probes", // curl localhost:10255/metrics/probes
+			url:      "http://localhost:10255/metrics/probes",
 			file:     "data/kubeletprobes",
 			report:   "report/kubelet-probes.log",
 		},
 		{
 			name:     "kube-controller-manager",
 			endpoint: "/metrics", // curl localhost:10252/metrics
+			url:      "http://localhost:10252/metrics",
 			file:     "data/kubecontrollermanager",
 			report:   "report/kube-controller-manager.log",
 		},
@@ -96,6 +105,26 @@ func main() {
 	}
 
 	for _, m := range ms {
+		if len(m.url) > 0 {
+			fmt.Printf("grabbing metrics from %s\n", m.url)
+			resp, err := http.Get(m.url)
+			if err != nil {
+				fmt.Printf("grabbing metrics from %s failed as %v\n", m.url, err)
+				continue
+			}
+
+			defer resp.Body.Close()
+			body, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				fmt.Printf("Read response body from %s failed as %v\n", m.url, err)
+				continue
+			}
+
+			if err = ioutil.WriteFile(m.file, body, os.ModePerm); err != nil {
+				fmt.Printf("Update data file failed for %s as %v\n", m.name, err)
+			}
+		}
+
 		fmt.Printf("Linting %s metrics from endpoint: %s\n", m.name, m.endpoint)
 
 		// Read metrics from file
